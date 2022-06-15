df2d48bb32f6bebe81c8566d3493ee5a81947813
Nutifafa Attor <attorfafa@gmail.com>
2022-06-15 17:37:31 +0000

    server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const databaseName = 'tinder-db'
+const connectionURL = `mongodb+srv://tinder-clone-admin:admin-tinder-clone@cluster0.k68qr.mongodb.net/?retryWrites=true&w=majority`
+
+// Middlewares
+
+// Database Config
+mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+// API Endpoints
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+})
+
+// Listener
+app.listen(port, () => {
+    console.log(`Server is listening on localhost: ${port}`);
+})
