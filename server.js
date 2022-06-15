151bb0b5544e7c8de1533257cac036ae2e37fca8
Nutifafa Attor <attorfafa@gmail.com>
2022-06-15 19:07:00 +0000

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import cards from "./dbCards.js";
+import Cors from "cors";
 
 // App Config
 const app = express();
@@ -8,6 +10,8 @@ const databaseName = 'tinder-db'
 const connectionURL = `mongodb+srv://tinder-clone-admin:admin-tinder-clone@cluster0.k68qr.mongodb.net/?retryWrites=true&w=majority`
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // Database Config
 mongoose.connect(connectionURL, {
@@ -21,7 +25,32 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
 })
 
-app.post("/tinder/cards", (req, res) => {})
+// Push info to the database
+app.post("/tinder/cards", (req, res) => {
+    const dbCard = req.body
+
+    cards.create(dbCard, (err, data) => {
+        if (err) {
+            // in case of error, return error message - internal server error
+            res.status(500).send(err)
+        } else {
+            // in case of success, return the card that was created
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    cards.find((err, data) => {
+        if (err) {
+            // in case of error, return error message - internal server error
+            res.status(500).send(err)
+        } else {
+            // in case of success, retrieve every thing from the database
+            res.status(200).send(data)
+        }
+    })
+})
 
 // Listener
 app.listen(port, () => {
